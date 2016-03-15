1de3fa9adeff918e0393627327a5d5811574ea89
raubrey2014 <rma7qb@virginia.edu>
2016-03-14 20:58:25 -0400

    First Commit

---
+++ theme/signup.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Office Space</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+  <script src="assets/js/Chart.js"></script>
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html"><i class="fa fa-circle"></i>ffice Space</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="about.html">ABOUT</a></li>
+            <li class='active'><a href="#">SIGN UP</a></li>
+
+           <!--  <li><a href="services.html">SERVICES</a></li>
+            <li><a href="works.html">WORKS</a></li> -->
+            <!-- <li><a data-toggle="modal" data-target="#myModal" href="#myModal"><i class="fa fa-envelope-o"></i></a></li> -->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+	<div id="blue">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-8 col-lg-offset-2">
+				<h1 id='special_h1'><strong>Buy Into Our Dynamic Business Solution</strong></h1>
+				</div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!--  bluewrap -->
+
+<?php
+
+
+
+$db = new mysqli('localhost', 'root', '', 'OfficeSpace2');
+if($db->connect_error){
+  die("Could not connect to db " . $db->connect_error);
+}
+
+
+if(isset($_POST["submit"])){
+  $first_name = $_POST["first_name"];
+  $last_name = $_POST["last_name"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $address = $_POST["address"];
+  $city = $_POST["city"];
+  $state = $_POST["state"];
+  $zip = $_POST["zip"];
+  
+
+  
+  
+
+  #check if email already is in use
+  $result = $db->query("SELECT * from Member where email='$email'");
+  $rowCount = $result->num_rows;
+  if($rowCount != 0){
+    // echo "An account with this email already exists!";
+  }else{
+
+    #insert maker into <makers> table
+    $query = "INSERT INTO Member (first_name, last_name, email, password, address, city, state, zip) values ('$first_name', '$last_name', '$email', '$password', '$address', '$city', '$state', '$zip')";
+    $res = $db->query($query) or die ("invalid: ". $db->error);
+    header("Location: index.html");
+    exit;
+    // #email confirmation 
+
+    // $mail = createMailer();
+    // $mail->addAddress($email);
+    // $mail->Subject = "Welcome $name!";
+    // $mail->Body = "Welcome, new maker $name! This is a confirmation email verifying your email address.";
+    // if(!$mail->send()){
+    //   echo 'error: message not sent<br>';
+    //   echo $mail->ErrorInfo;
+    // }
+    // else{
+    //   echo 'Please check your email for a message w/ your password';
+    // }
+
+
+  }
+
+}
+
+
+?>
+<div id="custom-wrapper2">
+	<div class="container w">
+    <!--REGISTRATION FORM HTML-->
+<div id="registration-form">
+  <div class='fieldset'>
+    <legend>Register Now</legend>
+    <form action="signup.php" method="post" onsubmit="return validateForm()" name="myForm">
+      <div class='row'>
+        <!-- <label for='first_name'>First Name</label> -->
+        <input type="text" placeholder="First Name" name='first_name' id='first_name' required>
+      </div>
+      <div class='row'>
+        <!-- <label for='last_name'>Last Name</label> -->
+        <input type="text" placeholder="Last Name" name='last_name' id='last_name' required>
+      </div>
+      <div class='row'>
+        <!-- <label for="email">E-mail</label> -->
+        <input type="email" placeholder="E-mail"  name='email' required>
+      </div>
+      <div class='row'>
+        <!-- <label for='address'>Address</label> -->
+        <input type="text" placeholder="Address" name='address' id='address' required>
+      </div>
+      <div class='row'>
+        <!-- <label for='city'>City</label> -->
+        <input type="text" placeholder="City" name='city' id='city' required>
+      </div>
+      <div class='row'>
+        <!-- <input type="text" maxlength="2" placeholder="State" onchange="validateState(this);"/> -->
+
+        <label for='state' id="spec">State</label>
+
+<select name="state">
+
+      <option value="AL">Alabama</option>
+      <option value="AK">Alaska</option>
+      <option value="AZ">Arizona</option>
+      <option value="AR">Arkansas</option>
+      <option value="CA">California</option>
+      <option value="CO">Colorado</option>
+      <option value="CT">Connecticut</option>
+      <option value="DE">Delaware</option>
+      <option value="DC">District Of Columbia</option>
+      <option value="FL">Florida</option>
+      <option value="GA">Georgia</option>
+      <option value="HI">Hawaii</option>
+      <option value="ID">Idaho</option>
+      <option value="IL">Illinois</option>
+      <option value="IN">Indiana</option>
+      <option value="IA">Iowa</option>
+      <option value="KS">Kansas</option>
+      <option value="KY">Kentucky</option>
+      <option value="LA">Louisiana</option>
+      <option value="ME">Maine</option>
+      <option value="MD">Maryland</option>
+      <option value="MA">Massachusetts</option>
+      <option value="MI">Michigan</option>
+      <option value="MN">Minnesota</option>
+      <option value="MS">Mississippi</option>
+      <option value="MO">Missouri</option>
+      <option value="MT">Montana</option>
+      <option value="NE">Nebraska</option>
+      <option value="NV">Nevada</option>
+      <option value="NH">New Hampshire</option>
+      <option value="NJ">New Jersey</option>
+      <option value="NM">New Mexico</option>
+      <option value="NY">New York</option>
+      <option value="NC">North Carolina</option>
+      <option value="ND">North Dakota</option>
+      <option value="OH">Ohio</option>
+      <option value="OK">Oklahoma</option>
+      <option value="OR">Oregon</option>
+      <option value="PA">Pennsylvania</option>
+      <option value="RI">Rhode Island</option>
+      <option value="SC">South Carolina</option>
+      <option value="SD">South Dakota</option>
+      <option value="TN">Tennessee</option>
+      <option value="TX">Texas</option>
+      <option value="UT">Utah</option>
+      <option value="VT">Vermont</option>
+      <option value="VA">Virginia</option>
+      <option value="WA">Washington</option>
+      <option value="WV">West Virginia</option>
+      <option value="WI">Wisconsin</option>
+      <option value="WY">Wyoming</option>
+    </select>
+      </div>
+      <script>
+
+</script>
+      <div class='row'>
+        <!-- <label for='zip'>Zip Code</label> -->
+        <input type="text" placeholder="Zip" name='zip' id='zip' required>
+      </div>
+      
+      <input type="submit" name="submit" value="Register">
+    </form>
+  </div>
+</div>
+		<!-- <form method = "POST" action = "signup.php" onsubmit="return validateForm()" name="myForm">
+  
+      <h1>Welcome! Please enter a name, valid email address and password to create maker account</h1>
+      <b>First Name: </b>
+      <b><input type = "text" name = "first_name" required></b><br>
+      <b>Last Name: </b>
+      <b><input type = "text" name = "last_name" required></b><br>
+      
+      <b>Email: </b>
+      <b><input type = "email" name = "email" required></b><br>
+      <b>Password: </b>
+      <b><input type = "password" name = "password" required></b><br>
+      <b>Address: </b>
+      <b><input type = "" name = "address" required></b><br>
+      <b>City: </b>
+      <b><input type = "" name = "city" required></b><br>
+      <b>State: </b>
+      <select name="state">
+      <option value="AL">Alabama</option>
+      <option value="AK">Alaska</option>
+      <option value="AZ">Arizona</option>
+      <option value="AR">Arkansas</option>
+      <option value="CA">California</option>
+      <option value="CO">Colorado</option>
+      <option value="CT">Connecticut</option>
+      <option value="DE">Delaware</option>
+      <option value="DC">District Of Columbia</option>
+      <option value="FL">Florida</option>
+      <option value="GA">Georgia</option>
+      <option value="HI">Hawaii</option>
+      <option value="ID">Idaho</option>
+      <option value="IL">Illinois</option>
+      <option value="IN">Indiana</option>
+      <option value="IA">Iowa</option>
+      <option value="KS">Kansas</option>
+      <option value="KY">Kentucky</option>
+      <option value="LA">Louisiana</option>
+      <option value="ME">Maine</option>
+      <option value="MD">Maryland</option>
+      <option value="MA">Massachusetts</option>
+      <option value="MI">Michigan</option>
+      <option value="MN">Minnesota</option>
+      <option value="MS">Mississippi</option>
+      <option value="MO">Missouri</option>
+      <option value="MT">Montana</option>
+      <option value="NE">Nebraska</option>
+      <option value="NV">Nevada</option>
+      <option value="NH">New Hampshire</option>
+      <option value="NJ">New Jersey</option>
+      <option value="NM">New Mexico</option>
+      <option value="NY">New York</option>
+      <option value="NC">North Carolina</option>
+      <option value="ND">North Dakota</option>
+      <option value="OH">Ohio</option>
+      <option value="OK">Oklahoma</option>
+      <option value="OR">Oregon</option>
+      <option value="PA">Pennsylvania</option>
+      <option value="RI">Rhode Island</option>
+      <option value="SC">South Carolina</option>
+      <option value="SD">South Dakota</option>
+      <option value="TN">Tennessee</option>
+      <option value="TX">Texas</option>
+      <option value="UT">Utah</option>
+      <option value="VT">Vermont</option>
+      <option value="VA">Virginia</option>
+      <option value="WA">Washington</option>
+      <option value="WV">West Virginia</option>
+      <option value="WI">Wisconsin</option>
+      <option value="WY">Wyoming</option>
+    </select><br>
+      <b>Zip Code: </b>
+      <b><input type = "text" name = "zip" required></b><br>
+      
+      
+      <b><input type = "submit" name = "submit" value = "enter site"></b><br>
+      <br>
+    </form> -->
+	</div><!-- container -->
+</div>
+
+
+
+
+	
+		<!-- FOOTER -->
+	<div id="f">
+		<div class="container">
+			<div class="row centered">
+				<div class="contact-cls">
+					<h1>CONTACT US</h1>
+                    <div>
+                        <span><i class="fa fa-home"></i>&nbsp; Address: Charlottesville, VA 29903</span>
+                        <span><i class="fa fa-phone"></i>&nbsp;Phone: +1 (540) 250 - 3750</span>
+                        <span><i class="fa fa-envelope-o"></i>&nbsp;E-Mail: officeSpace@gmail.com</span>
+                        <span><i class="fa fa-globe"></i>&nbsp;Web: www.officeSpace.com</span>
+
+                    </div>
+                  
+                </div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!-- Footer -->
+
+	
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript">
+    function validateForm() {
+      var arr = new Array();
+      var re = /^[A-Za-z]+$/;
+
+      var first_name = document.forms["myForm"]["first_name"].value;
+      if(!re.test(first_name))
+         arr.push("Please use only A-Za-z characters on field: First Name.")
+
+      var last_name = document.forms["myForm"]["last_name"].value;
+      if(!re.test(last_name))
+         arr.push("Please use only A-Za-z characters on field: Last Name.")
+      
+      var email = document.forms["myForm"]["email"].value;
+      if((email.indexOf(".") == -1))
+         arr.push("Please include an email address of the format: email@example.com.")
+
+//       var state = document.forms["myForm"]["state"].value;
+//       var b = false;
+//       var states =    ["AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE",
+// "FL","GA","GU","HI","IA",
+// "ID","IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT",
+// "NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","PW","RI",
+// "SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
+//     for(var i=0;i< states.length;i++) {
+//       if(state.toUpperCase() == states[i]) {
+//         b = true;
+//       }
+//     }
+//     if(!b){
+//          arr.push("Please include a valid state extension for field: State.")
+
+//     }
+
+       if (arr.length > 0){
+        var str = arr.join("\n");        
+        alert(str);
+        return false;
+      }
+      
+    }
+
+    </script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
